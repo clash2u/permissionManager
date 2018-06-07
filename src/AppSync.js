9a7b602a10be907cc91c305f1f8f21a00f74741e
clash2u <clash2u@gmail.com>
2018-06-07 16:18:13 +0900

    API Key update

--- src/AppSync.js
+++ src/AppSync.js
@@ -2,5 +2,5 @@
     "graphqlEndpoint": "https://3idgznb43zcdnonv47s5tf6rmi.appsync-api.us-east-1.amazonaws.com/graphql",
     "region": "us-east-1",
     "authenticationType": "API_KEY",
-    "apiKey": "da2-lotubj6jajduldni5zyiihlghq"
+    "apiKey": "da2-b4d74y34vjbe3dbgfq6txw5zdy"
 }
